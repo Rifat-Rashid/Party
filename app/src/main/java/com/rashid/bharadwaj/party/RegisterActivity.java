35a82a7e7c3ddabb16e89ec4878b3d40220e838d
tejaskb04 <tejaskb@gmail.com>
2017-10-20 14:34:22 -0700

    Register Skeleton

--- app/src/main/java/com/rashid/bharadwaj/party/RegisterActivity.java
+++ app/src/main/java/com/rashid/bharadwaj/party/RegisterActivity.java
@@ -3,11 +3,16 @@ package com.rashid.bharadwaj.party;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private LinearLayout registerLayout;
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,10 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
         registerLayout = (LinearLayout) findViewById(R.id.register_layout);
+        firstNameEditText = (EditText) findViewById(R.id.first_name);
+        lastNameEditText = (EditText) findViewById(R.id.last_name);
+        emailEditText = (EditText) findViewById(R.id.email);
+        passwordEditText = (EditText) findViewById(R.id.password);
         registerLayout.setBackgroundResource(R.drawable.party_background_1);
     }
 }
