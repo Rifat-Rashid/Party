2b2ba2c8a9c548517211ff0b6c2a4f266475828e
tejaskb04 <tejaskb@gmail.com>
2017-10-20 13:10:17 -0700

    Register Skeleton

--- app/src/main/java/com/rashid/bharadwaj/party/RegisterActivity.java
+++ app/src/main/java/com/rashid/bharadwaj/party/RegisterActivity.java
@@ -2,12 +2,21 @@ package com.rashid.bharadwaj.party;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private LinearLayout registerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
+        registerLayout = (LinearLayout) findViewById(R.id.register_layout);
+        registerLayout.setBackgroundResource(R.drawable.party_background_1);
     }
 }
