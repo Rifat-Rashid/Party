270716fde62ccb8be1b937c6fca34cd92a1ba55a
tejaskb04 <tejaskb@gmail.com>
2017-10-20 18:07:07 -0700

    Register Logic

--- app/src/main/java/com/rashid/bharadwaj/party/RegisterActivity.java
+++ app/src/main/java/com/rashid/bharadwaj/party/RegisterActivity.java
@@ -1,18 +1,36 @@
 package com.rashid.bharadwaj.party;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
     private LinearLayout registerLayout;
     private EditText firstNameEditText;
     private EditText lastNameEditText;
     private EditText emailEditText;
     private EditText passwordEditText;
+    private Button loginRouteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +39,53 @@ public class RegisterActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         registerLayout = (LinearLayout) findViewById(R.id.register_layout);
         firstNameEditText = (EditText) findViewById(R.id.first_name);
         lastNameEditText = (EditText) findViewById(R.id.last_name);
         emailEditText = (EditText) findViewById(R.id.email);
         passwordEditText = (EditText) findViewById(R.id.password);
+        loginRouteBtn = (Button) findViewById(R.id.login_route);
         registerLayout.setBackgroundResource(R.drawable.party_background_1);
+        loginRouteBtn.setBackgroundColor(Color.parseColor("#FFA500"));
+        loginRouteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void registerUser() {
+        final String firstName = firstNameEditText.getText().toString();
+        final String lastName = lastNameEditText.getText().toString();
+        final String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        if (TextUtils.isEmpty(firstName) || TextUtils.isEmpty(lastName) || TextUtils.isEmpty(email)
+                || TextUtils.isEmpty(password)) {
+            Toast.makeText(RegisterActivity.this, "There are one or more empty fields.",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                                String userId = firebaseUser.getUid();
+                                User user = new User(firstName, lastName, email);
+                                databaseReference.child("users").child(userId).setValue(user);
+                                Toast.makeText(RegisterActivity.this, "Registered Successfully",
+                                        Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(RegisterActivity.this, DashboardActivity.class));
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Failed to Register",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
---
+++ app/src/main/java/com/rashid/bharadwaj/party/User.java
@@ -0,0 +1,26 @@
+package com.rashid.bharadwaj.party;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
