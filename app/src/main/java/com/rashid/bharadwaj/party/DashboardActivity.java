ddf897d7b2a32b56b5bcd38b187a938c1ba88741
DevWork <s-rrashid@lwsd.org>
2017-10-20 17:53:27 -0700

    init

--- app/src/main/java/com/rashid/bharadwaj/party/DashboardActivity.java
+++ app/src/main/java/com/rashid/bharadwaj/party/DashboardActivity.java
@@ -3,11 +3,17 @@ package com.rashid.bharadwaj.party;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
 public class DashboardActivity extends AppCompatActivity {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //Mapbox.getInstance(this,)
     }
 }
